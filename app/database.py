bfe1d8b858790022bae1ee4612593c62da4054fe
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-09-15 23:35:57 +0300

    if the db does not exist, create it

--- app/database.py
+++ app/database.py
@@ -1,14 +1,38 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
+import psycopg2
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
 
-engine = create_engine(DATABASE_URL)
+# Create DB if not exists
+def create_db_if_not_exists():
+    conn = psycopg2.connect(
+        dbname='postgres',
+        user=DATABASE_USERNAME,
+        password=DATABASE_PASSWORD,
+        host=DATABASE_URL.replace('http://', '').replace('https://', '')
+    )
+    conn.autocommit = True
+    cur = conn.cursor()
+    cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DATABASE_NAME}';")
+    if not cur.fetchone():
+        cur.execute(f'CREATE DATABASE {DATABASE_NAME};')
+    conn.close()
+
+create_db_if_not_exists()
+
+# SQLAlchemy DB Connection
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_URL}/{DATABASE_NAME}"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
