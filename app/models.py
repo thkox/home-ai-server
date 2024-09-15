1803e8dba56c5bcc637a4fdfd7cfecb2dcb452f9
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-09-15 23:36:14 +0300

    update roles and user model

--- app/models.py
+++ app/models.py
@@ -1,17 +1,21 @@
-from sqlalchemy import Column, Integer, String, Enum
+import uuid
+from sqlalchemy import Column, Integer, String, Boolean, Enum
+from sqlalchemy.dialects.postgresql import UUID
 from .database import Base
 import enum
 
 class UserRole(str, enum.Enum):
     admin = "admin"
-    user = "user"
+    house_member = "house_member"
 
 class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, index=True)
     first_name = Column(String, index=True)
     last_name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    role = Column(Enum(UserRole), default=UserRole.user)
+    role = Column(Enum(UserRole), default=UserRole.house_member)
+    enabled = Column(Boolean, default=True)  # New field to indicate if user is active
