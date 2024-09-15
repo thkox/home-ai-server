ab9f485f70c9e1b9970053042371816a854eb4cd
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-09-15 23:35:37 +0300

    pass in the get_current_user the first_name, last_name and user_id

--- app/auth.py
+++ app/auth.py
@@ -16,11 +16,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# Utility functions
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
@@ -52,20 +50,19 @@ def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_
     
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
+        user_id: str = payload.get("user_id")
+        first_name: str = payload.get("first_name")
+        last_name: str = payload.get("last_name")
+        if user_id is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        token_data = TokenData(user_id=user_id, first_name=first_name, last_name=last_name)
     except JWTError:
         raise credentials_exception
 
-    user = get_user(db, email=token_data.username)
+    user = db.query(User).filter(User.user_id == token_data.user_id).first()
     if user is None:
         raise credentials_exception
     return user
-
-def get_current_active_user(current_user: User = Depends(get_current_user)):
-    return current_user
 
 def get_current_admin_user(current_user: User = Depends(get_current_user)):
     if current_user.role != UserRole.admin:
